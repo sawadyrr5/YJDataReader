e61f96c4189e370c4b61b3ebe6a8eadbca3cff5d
RR4 <riskreturn5@gmail.com>
2017-02-05 14:13:31 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='YahooJapanDataReader',
+    version='0.0.1',
+    description='Read japanese stock price and corporate profile data from Yahoo! Finance Japan.',
+    author='sawadyrr5',
+    author_email='riskreturn5@gmail.com',
+    url='https://github.com/sawadyrr5/YahooJapanDataReader',
+    packages=['YahooJapanDataReader'],
+    )
